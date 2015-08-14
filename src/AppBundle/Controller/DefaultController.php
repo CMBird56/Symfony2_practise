046e1a127235c2ccfbf1220d12d92a781f5ab3ed
unknown <vezbe@RC-12A.ETF.LOCAL>
2015-08-14 10:19:51 +0200

    ~vracanje na status quo pre eksperimentisanja

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Task;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,35 +11,11 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function newAction(Request $request)
+    public function indexAction(Request $request)
     {
-
-        $task = new Task();
-
-        $form = $this->createFormBuilder($task)
-            ->add('task', 'text')
-            ->add('dueDate', 'date')
-            ->add('save', 'submit', array('label' => 'Create Task'))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()) {
-            // perform some action, such as saving the task to the database
-            $var = $form->get('task')->getData();
-            echo "<p>" . $var . "</p>" . "<br/>";
-
-        }
-
-        /*if($form->isValid()) {
-            return $this->redirectToRoute('task_success');
-        }*/
-
-        return $this->render('default/new.html.twig', array(
-            'form' => $form->createView(),
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
-
     }
-
-
 }
